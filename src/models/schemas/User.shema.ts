68f25e2da1daa912bb560d37f638c4b0696091f3
anhvuDev103 <anhvu.dev.103@gmail.com>
2024-09-06 09:06:16 +0700

    chore: update user schema

--- src/models/schemas/User.shema.ts
+++ src/models/schemas/User.shema.ts
@@ -2,20 +2,30 @@ import { ObjectId } from 'mongodb';
 
 type UserContructor = {
   _id?: ObjectId;
+
   name: string;
   email: string;
   password: string;
   description?: string;
+
+  forget_password_token?: string;
+  otp_code?: string;
+
   created_at?: Date;
   updated_at?: Date;
 };
 
 class User {
   _id: ObjectId;
+
   name: string;
   email: string;
   password: string;
   description: string;
+
+  forget_password_token: string;
+  otp_code: string;
+
   created_at: Date;
   updated_at: Date;
 
@@ -23,10 +33,15 @@ class User {
     const now = new Date();
 
     this._id = payload._id || new ObjectId();
+
     this.name = payload.name;
     this.email = payload.email;
     this.password = payload.password;
     this.description = payload.description || '';
+
+    this.forget_password_token = payload.forget_password_token || '';
+    this.otp_code = payload.otp_code || '';
+
     this.created_at = payload.created_at || now;
     this.updated_at = payload.updated_at || now;
   }
