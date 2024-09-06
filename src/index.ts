ee6be3aaf66eb027aa99c222a86b83f378de1720
anhvuDev103 <anhvu.dev.103@gmail.com>
2024-09-06 09:58:37 +0700

    chore: connect database mongo

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
+import databaseService from './services/database.services';
+
 const app = express();
 const port = process.env.PORT;
+
+databaseService.connect().catch(console.dir);
 
 app.use(express.json());
 app.use(cors());
---
+++ src/services/database.services.ts
@@ -0,0 +1,30 @@
+import { Collection, Db, MongoClient } from 'mongodb';
+
+import User from '@/models/schemas/User.shema';
+
+const CONNECTION_UI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@clickup.fabew.mongodb.net/?retryWrites=true&w=majority&appName=clickup`;
+
+class DatabaseService {
+  private client: MongoClient;
+  private db: Db;
+
+  constructor() {
+    this.client = new MongoClient(CONNECTION_UI);
+    this.db = this.client.db(process.env.MONGO_DB_NAME);
+  }
+
+  async connect() {
+    await this.client.connect();
+
+    await this.db.command({ ping: 1 });
+    console.log('You successfully connected to MongoDB!');
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.MONGO_USERS_COLLECTION_NAME as string);
+  }
+}
+
+const databaseService = new DatabaseService();
+
+export default databaseService;
