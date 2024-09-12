6ecbb06b46a305e43c18819ff3f07c718e2291ef
anhvuDev103 <anhvu.dev.103@gmail.com>
2024-09-12 11:25:50 +0700

    chore: comment description for function and route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,6 +6,21 @@ import { wrapRequestHandler } from '@/utils/error-handler';
 
 const userRouter = express.Router();
 
+/**
+ * POST /users/sign-up
+ *
+ * Request body:
+ * {
+ *    name: string
+ *    email: string
+ *    password: string
+ * }
+ *
+ * Response:
+ * - 201 Created: On success.
+ * - 422 Unprocessable Entity: When input data is invalid.
+ * - 409 Conflict: When the email already exists.
+ */
 userRouter.post('/sign-up', signUpValidator, wrapRequestHandler(signUpController));
 
 userRouter.post('/sign-in', signInValidator, wrapRequestHandler(signInController));
--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,3 +1,5 @@
+import { InsertOneResult } from 'mongodb';
+
 import { SignUpRequestBody } from '@/models/requests/user.requests';
 import User from '@/models/schemas/User.shema';
 import { generateOTP } from '@/utils/common';
@@ -6,7 +8,19 @@ import { hashPassword } from '@/utils/crypto';
 import databaseService from './database.services';
 
 class UserService {
-  async signUp(payload: SignUpRequestBody) {
+  /**
+   * Registers a new user in the system.
+   *
+   * @param {Object} payload - An object containing user sign-up information.
+   * @param {string} payload.name - The name provided by the user.
+   * @param {string} payload.email - The unique email address.
+   * @param {string} payload.password - The user's password.
+   *
+   * @returns {Promise<InsertOneResult<User>>} - A promise that resolves with the created user object if successful.
+   *
+   * @throws {Error} if any database side errors occur.
+   */
+  async signUp(payload: SignUpRequestBody): Promise<InsertOneResult<User>> {
     //TODO: send OTP to user's email
 
     const result = await databaseService.users.insertOne(
@@ -21,6 +35,21 @@ class UserService {
   }
 
   async signIn() {}
+
+  /**
+   * Checks if an email already exists in the database.
+   *
+   * @param {string} email - The email address to check.
+   *
+   * @returns {Promise<boolean>} - A promise that resolves with the created user object if successful.
+   *
+   * @throws {Error} if any database side errors occur.
+   */
+  async checkIfEmailExists(email: string) {
+    const existingUser = await databaseService.users.findOne({ email });
+
+    return Boolean(existingUser);
+  }
 }
 
 const userService = new UserService();
