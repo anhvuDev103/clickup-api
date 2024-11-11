5475fda97cc92ebfda2c771a300856e724eb379c
anhvuDev103 <anhvu.dev.103@gmail.com>
2024-11-11 13:50:37 +0700

    fix: error handle logs

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
+import _ from 'lodash';
 
 import { HttpStatus } from '@/constants/enums';
 import { BaseError } from '@/models/Errors.model';
@@ -16,7 +17,7 @@ export const wrapRequestHandler = <P>(requestHandler: RequestHandler<P>) => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const defaultErrorRequestHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof BaseError) {
-    return res.status(err.status).json(err);
+    return res.status(err.status).json(_.omit(err, 'details'));
   }
 
   Object.getOwnPropertyNames(err).forEach((key) => {
@@ -30,5 +31,5 @@ export const defaultErrorRequestHandler = (err: any, _req: Request, res: Respons
     message: err.message,
   });
 
-  return res.status(HttpStatus.InternalServerError).json(error);
+  return res.status(HttpStatus.InternalServerError).json(_.omit(error, 'details'));
 };
